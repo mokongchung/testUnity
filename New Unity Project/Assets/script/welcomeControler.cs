6f740c7ea00fe80ca94cdc8a89b4576a2d7c5f60
Lê Doãn Thien Phúc <phuc.ledoanthien@gameloft.com>
2020-11-12 18:27:13 +0700

    fix bug build file can't read json

--- New Unity Project/Assets/script/welcomeControler.cs
+++ New Unity Project/Assets/script/welcomeControler.cs
@@ -3,25 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
 using UnityEngine.UI;
-
+using System.IO;
 public class welcomeControler : MonoBehaviour
 {
     // Start is called before the first frame update
-    public TextAsset jsonFile;
+
 	public Button _buttonPlayList1;
 	public Button _buttonPlayList2;
 	public Button _buttonPlayList3;
     void Start()
     {
-        List<Playlist> list = JSONReader.readJson(jsonFile.text);
+        var fileContents = File.ReadAllText(Application.streamingAssetsPath + "/coding-test-frontend-unity.json");
+        List<Playlist> list = JSONReader.readJson(fileContents.ToString());
 		_buttonPlayList1.GetComponentInChildren<Text>().text = list[0].playlist;
 		_buttonPlayList2.GetComponentInChildren<Text>().text = list[1].playlist;
 		_buttonPlayList3.GetComponentInChildren<Text>().text = list[2].playlist;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
